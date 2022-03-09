7ed463986b9f601e6b017663eae62028a4a77d22
darawanlertsirisakhon <cakedrw@gmail.com>
2022-03-09 17:15:23 +0700

    Update nuxt.config.js
    static

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,8 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-  mode:'universal',target:'server',
+  mode:'universal',
+  target:'static',
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
