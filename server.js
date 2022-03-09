79eb44358357d90e62464da22bfc89dfa9d260d4
darawanlertsirisakhon <cakedrw@gmail.com>
2022-03-09 17:58:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const app = express();
 const router = express.Router();
-const port = 3000;
+const port = process.env.PORT|| 3000;
 app.use(cors());
 app.use(express.json());
 
